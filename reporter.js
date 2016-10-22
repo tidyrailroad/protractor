df2442e2262d7899a9d9106eb82d52429a832297
Emory Merryman <emory.merryman@gmail.com>
2016-10-22 23:51:46 +0000

    recordmydesktop

--- reporter.js
+++ reporter.js
@@ -1,6 +1,9 @@
 (function(){
     "use strict";
     module.exports = function() {
+        var spawn = require("child-proces-promise").spawn();
+        var recordMyDesktop;
+        
         this.jasmineStarted = function(){
             console.log("EXCITED");
             console.log(arguments);
@@ -9,11 +12,15 @@
         this.specStarted = function() {
             console.log("SPEC STARTED") ;
             console.log(arguments);
+            spawn("recordmydesktop", ["--display", ":99", "-0", "/protractor/reports/out.ogv", "--no-sound", "--on-the-fly-encoding"]).progress(function(success){
+                recordMyDesktop = success;
+            });
         };
         
         this.specDone = function(result) {
             console.log("SPEC FINISHED");
-            console.log(arguments);
+            console.log([arguments, recordMyDesktop]);
+            spawn("kill", ["SIGINT", recordMyDesktop.pid])
         };
     };
 }());
