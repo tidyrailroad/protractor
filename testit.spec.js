9fa71741d2fd1addb093b8d2ea3086fc100913d7
Emory Merryman <emory.merryman@gmail.com>
2016-10-22 20:50:14 +0000

    wtf

--- testit.spec.js
+++ testit.spec.js
@@ -2,7 +2,7 @@
     "use strict";
     describe("it", function(){
         beforeEach(function(){
-            browser.get();
+            browser.get("tutorial");
         });
         it("works", function(){
             expect(element.all(by.tagName("h2")).count()).toEqual(5);
