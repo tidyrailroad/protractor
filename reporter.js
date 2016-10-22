475e51ef49bec611387688a90000fb3a6a74444a
Emory Merryman <emory.merryman@gmail.com>
2016-10-22 23:16:42 +0000

    add spec reporter

--- reporter.js
+++ reporter.js
@@ -2,6 +2,12 @@
     "use strict";
     module.exports = function() {
         this.jasmineStarted = function(){
+            console.log("EXCITED");
+            console.log(arguments);
+        };
+        
+        this.specStarted = function() {
+            console.log("SPEC STARTED") ;
             console.log(arguments);
         };
     };
