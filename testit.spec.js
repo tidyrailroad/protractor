81fbdeeeeb7a42ea2d434b93c52cb4c786d638c4
Emory Merryman <emory.merryman@gmail.com>
2016-10-22 19:49:24 +0000

    launch browser

--- testit.spec.js
+++ testit.spec.js
@@ -1,6 +1,9 @@
 (function(){
     "use strict";
     describe("it", function(){
+        beforeEach(function(){
+            browser.get();
+        });
         it("works", function(){
             expect(element.all(by.tagName("h2")).count()).toEqual(5);
         });
