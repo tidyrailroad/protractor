0f579f4dcde679c04210dc91c0c5b2cca7c923a7
Emory Merryman <emory.merryman@gmail.com>
2016-10-24 14:47:37 +0000

    seems to work

--- conf.js
+++ conf.js
@@ -7,16 +7,16 @@
     });
     module.exports.config = {
         directConnect: true,
+        /*
         capabilities: {
             'browserName': 'firefox'
         },
-        /*
+        */
         multiCapabilities: [
             {
                 'browserName': 'firefox'
             }
         ],
-        */
         specs: ['specs/**/*.js'],
         jasmineNodeOpts: {
             showColors: true, // Use colors in the command line report.
@@ -27,15 +27,14 @@
         },
         onPrepare: function() {
             var Reporter = require("./reporter.js");
-            jasmine.getEnv().addReporter(new Reporter());
-            /*
+//            jasmine.getEnv().addReporter(new Reporter());
             browser.getCapabilities().then(function(caps) {
+                console.log(caps);
                 var browserName = caps.browserName;
                 var reporter = new Reporter();
                 reporter.browser = browserName;
-                jasmine.getEnv().addReporter(reporter);
+               jasmine.getEnv().addReporter(reporter);
             });
-            */
             browser.ignoreSynchronization = true;
         },
         onComplete: function() {},
--- reporter.js
+++ reporter.js
@@ -7,7 +7,7 @@
         var recordMyDesktop;
         
         this.specStarted = function(spec) {
-            console.log("specStarted:  ");
+            console.log("spec started");
             spawn("recordmydesktop", ["-display", ":99", "-o", require("path").join("/", "protractor", "report", this.browser+"_"+spec.fullName.replace(/\s/g,"_")), "--no-sound", "--on-the-fly-encoding"]).progress(function(progress){
                 if(progress===null){
                     console.log("PROGRESS IS NULL!");
