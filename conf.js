385a98e1193d5d0523fe73a78b4b768b34d6769f
Emory Merryman <emory.merryman@gmail.com>
2016-10-24 15:26:54 +0000

    works

--- conf.js
+++ conf.js
@@ -7,16 +7,9 @@
     });
     module.exports.config = {
         directConnect: true,
-        /*
         capabilities: {
             'browserName': 'firefox'
         },
-        */
-        multiCapabilities: [
-            {
-                'browserName': 'firefox'
-            }
-        ],
         specs: ['specs/**/*.js'],
         jasmineNodeOpts: {
             showColors: true, // Use colors in the command line report.
@@ -26,16 +19,9 @@
             xvfb.startSync();
         },
         onPrepare: function() {
-            var Reporter = require("./reporter.js");
-//            jasmine.getEnv().addReporter(new Reporter());
-            browser.getCapabilities().then(function(caps) {
-                console.log(caps);
-                var browserName = caps.browserName;
-                var reporter = new Reporter();
-                reporter.browser = browserName;
-               jasmine.getEnv().addReporter(reporter);
-            });
             browser.ignoreSynchronization = true;
+            var Reporter = require("./reporter.js");
+            jasmine.getEnv().addReporter(new Reporter());
         },
         onComplete: function() {},
         onCleanup: function() {},
--- reporter.js
+++ reporter.js
@@ -3,23 +3,18 @@
     module.exports = function() {
         var spawn = require("child-process-promise").spawn;
         var file;
-        var say = require("say");
         var recordMyDesktop;
         
         this.specStarted = function(spec) {
-            console.log("spec started");
-            spawn("recordmydesktop", ["-display", ":99", "-o", require("path").join("/", "protractor", "report", this.browser+"_"+spec.fullName.replace(/\s/g,"_")), "--no-sound", "--on-the-fly-encoding"]).progress(function(progress){
+            spawn("recordmydesktop", ["-display", ":99", "-o", require("path").join("/", "protractor", "report", spec.fullName.replace(/\s/g,"_")), "--no-sound", "--on-the-fly-encoding"]).progress(function(progress){
                 if(progress===null){
                     console.log("PROGRESS IS NULL!");
                 }
                 recordMyDesktop = progress;
-                say.speak(spec.description);
-            });
+             });
          };
         
         this.specDone = function(result) {
-            console.log("SPEC FINISHED:  "+this.browser);
-            console.log([arguments, recordMyDesktop]);
             spawn("kill", ["SIGINT", recordMyDesktop.pid])
         };
     };
